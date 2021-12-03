12f9566ab0cc8d4903d056fa1e4b7272cccdfa21
flaChang <zza46585@163.com>
2021-12-04 00:15:56 +0800

    样式初成

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,23 +1,43 @@
 <template>
   <div>
-  <Topnav/>
+    <Topnav />
     <div class="banner">
       <h1>Dora-UI</h1>
       <h2>一个基于vue 3的自制框架</h2>
       <p class="actions">
-        <a href="">github</a>
-        <a href="">开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
 </template>
 
-<script lang='ts'>
-import Topnav from '../components/Topnav.vue'
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
 export default {
-    components: {Topnav}
-}
+  components: { Topnav },
+};
 </script>
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
