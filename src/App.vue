6f955472ced991e84f2993da670beedd0a47716b
flaChang <zza46585@163.com>
2021-12-03 11:46:24 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Dora/>
+</template>
+
+<script>
+import Dora from './components/dora.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dora
+  }
+}
+</script>
