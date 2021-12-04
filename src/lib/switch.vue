005b899b40e78015acf7c4484df149f6351e11e3
flaChang <zza46585@163.com>
2021-12-05 00:07:39 +0800

    优化变量名称以及css

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script lang="ts">
-
 export default {
   props: {
     value: Boolean,
@@ -31,25 +30,24 @@ button {
   background: grey;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-
-button.checked {
-  background: blue;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+  &.checked {
+    background: blue;
+  }
+  &.checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+  &:focus {
+    outline: none;
+  }
 }
 </style>
