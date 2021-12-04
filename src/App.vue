7eb100ec21259dd34de55e798071b72343d32f62
flaChang <zza46585@163.com>
2021-12-04 15:14:21 +0800

    官网搭建

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
 </template>
 
 <script lang="ts">
-import { ref, provide } from 'vue'
+import { ref, provide } from "vue";
+import { router } from "./router";
 export default {
   name: "App",
   setup() {
-    const menuVisible = ref(false);
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
     provide("menuVisible", menuVisible); // set
+    router.afterEach(() => {
+      if(width < 500) {
+        menuVisible.value = false;
+      }
+    });
   },
 };
 </script>
