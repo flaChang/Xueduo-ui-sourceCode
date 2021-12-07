6416070c79eccac2ae8e22666792fd3f60cc7d18
flaChang <zza46585@163.com>
2021-12-07 20:24:40 +0800

    button component format

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,23 +2,66 @@
   <h2>button示例</h2>
   <div class="button-page">示例1 基本样式</div>
   <div>
-    <Button>确定</Button>
+    
+    <Button theme="button">确定</Button>
     <Button theme="link">确定</Button>
     <Button theme="text">确定</Button>
-    <Button theme="button">确定</Button>
   </div>
+  <div class="button-page">示例2 不同大小</div>
+  <div>
+    <Button size="big">确定</Button>
+    <Button>确定</Button>
+    <Button size="small">确定</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">确定</Button>
+    <Button theme="link">确定</Button>
+    <Button theme="link" size="small" >确定</Button>
+  </div>
+  <div>
+    <Button size="big" theme="text">确定</Button>
+    <Button theme="text">确定</Button>
+    <Button theme="text" size="small" >确定</Button>
+  <div class="button-page">示例3 优先级</div>
+  <div>
+    <Button level='main'>提醒</Button>
+    <Button >正常</Button>
+    <Button level='danger'>危险</Button>
+  </div>
+  <div>
+    <Button theme="link" level='main'>提醒</Button>
+    <Button theme="link">正常</Button>
+    <Button theme="link" level='danger'>危险</Button>
+  </div>
+  <div>
+    <Button theme="text" level='main'>提醒</Button>
+    <Button theme="text">正常</Button>
+    <Button  theme="text" level='danger'>危险</Button>
+  </div>
+  <div class="button-page">示例4 disabled</div>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <div class="button-page">示例5 加载按钮</div>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
+
 </template>
 
 <script lang="ts">
 import Button from "../lib/button.vue";
 export default {
   components: { Button },
- 
 };
 </script>
 
 <style lang="scss">
-  .button-page{
+.button-page {
+  margin-top: 10px;
   line-height: 35px;
-  }
+}
 </style>
--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,16 +1,45 @@
 <template>
-  <button class="gulu-button" :class="{ [`gulu-theme-${theme}`]: theme }">
+  <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="gulu-loadingIndicator"></span>
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+     loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      };
+    });
+    return { classes };
   },
 };
 </script>
@@ -18,7 +47,12 @@ export default {
 <style lang="scss">
 $h: 44px;
 $h2: 190px;
+$grayWhite: rgb(204, 204, 204);
+$gray: rgb(0, 105, 200);
+$softBlue: rgb(0, 120, 215);
+$softWhite: rgb(225, 225, 225);
 $color: #333;
+$alert: rgb(254, 189, 56);
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -29,20 +63,99 @@ $color: #333;
   justify-content: center;
   align-items: center;
   white-space: nowrap;
-  background: rgb(204, 204, 204);
+  background: $grayWhite;
   color: $color;
   border: none;
-  &  {
-    margin-left: 8px;
+  & {
+    margin-left: 20px;
     margin-top: 8px;
     @media (max-width: 500px) {
-      margin-left: 0px; 
+      margin-left: 0px;
     }
   }
- 
+  &.gulu-theme-link {
+    background-color: #fff;
+    color: $gray;
+    &:hover,
+    &:focus {
+      color: lighten($gray, 20%);
+      border: none;
+    }
+  }
+  &.gulu-theme-text {
+    background-color: #fff;
+    color: $color;
+    &:hover,
+    &:focus {
+      border: none;
+    }
+  }
+  &.gulu-theme-button,
+  &.gulu-theme-text,
+  &.gulu-theme-link {
+    &.gulu-size-big {
+      font-size: 20px;
+      height: 55px;
+      padding: 0 16px;
+    }
+    &.gulu-size-small {
+      font-size: 12px;
+      height: 35px;
+      padding: 10px;
+    }
+  }
+  &.gulu-theme-button {
+    &.gulu-level-main {
+      border: 2.5px solid $softBlue;
+      background-color: $softWhite;
+      &:hover,
+      &:focus {
+        background-color: rgb(229, 241, 251);
+        border: 2px solid rgb(155, 202, 239);
+        border-bottom: 2px solid rgb(33, 137, 219);
+      }
+    }
+    &.gulu-level-danger {
+      background-color: $alert;
+      color: $gray;
+    }
+    &[disabled] {
+      border:1px solid rgb(191,191,191);
+      cursor: not-allowed;
+      color: rgb(131, 131, 131);
+    }
+  }
+  &.gulu-theme-text,
+  &.gulu-theme-link {
+    &.gulu-level-main {
+      color: rgb(33, 137, 219);
+    }
+    &.gulu-level-danger {
+      color: $alert;
+      &:hover,
+      &:focus {
+        color: $gray;
+      }
+    }
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grayWhite;
+    }
+  }
+> .gulu-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $gray $gray $gray transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
   &:hover,
   &:focus {
-    border: 2.5px solid rgb(124, 124, 124);
+    border: 2.5px solid $gray;
   }
   &:focus {
     outline: none;
@@ -50,5 +163,9 @@ $color: #333;
   &::-moz-focus-inner {
     border: 0;
   }
+}
+@keyframes gulu-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>
